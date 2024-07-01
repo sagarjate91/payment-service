00bf6e1c3012c2b79f9a31180f68a39af2c464f8
Sagar Jate <jatekomal@gmail.com>
2024-07-01 10:46:45 +0530

    eureka server is added

---
+++ src/main/java/com/training/payment_service/exception/NoDataFoundException.java
@@ -0,0 +1,8 @@
+package com.training.payment_service.exception;
+
+public class NoDataFoundException extends Exception{
+
+    public NoDataFoundException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/training/payment_service/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.training.payment_service.service;
+
+import com.training.payment_service.entity.Payment;
+
+public interface PaymentService {
+
+    Payment paymentSave(Payment payment);
+
+}
