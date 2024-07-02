9177036c1cbfe26f3a59c4fb143026fb47c02ad7
Sagar Jate <jatekomal@gmail.com>
2024-07-02 12:27:10 +0530

    identity id is added

--- src/main/java/com/training/payment_service/entity/Payment.java
+++ src/main/java/com/training/payment_service/entity/Payment.java
@@ -1,9 +1,6 @@
 package com.training.payment_service.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +15,7 @@ import java.util.UUID;
 public class Payment {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer paymentId;
     private String paymentStatus;
     private String transactionId= UUID.randomUUID().toString();
